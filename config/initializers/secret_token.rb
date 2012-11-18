c759fc5fbaf3a0132ac0abaec2032398e6c702f7
Jason Rylance <ecnalyr@gmail.com>
2012-11-18 16:58:28 -0600

    Initial Commit with files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplestore::Application.config.secret_token = '11917ee9cba886f009df3af7e597a83f510463fca9c2b785fe68f47184966ebc89e1b69cca9e847cf86549771a9110f02c1ba45986518f910e5727bdf13edbbf'
